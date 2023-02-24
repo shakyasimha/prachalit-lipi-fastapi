81fa99794a1fcac06ee44161c77bbcd0895f9eab
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-02-24 15:16:00 +0545

    added docker file and few changes to main.py

--- main.py
+++ main.py
@@ -1,25 +1,72 @@
 from fastapi import FastAPI, UploadFile
+import uvicorn
 import tensorflow as tf
 from tensorflow import keras
+
+
+# Some constants to be used in the program
+IMG_SIZE = (32,32)
+APP_HOST = '0.0.0.0'
+APP_PORT = '5000'
+
+
+
 
 # Importing the model
 model = keras.saved_model.load('vgg16-prachalit/')
 
-app = FastAPI()
-
-@app.post('/predict_image')
-async def predict_image(image: UploadFile):
-    # Image reading
-    image_bytes = await image.read()
-    input_tensor = tf.image.decode_image(image_bytes)
-    input_tensor = tf.image.conver_image_dtype(input_tensor, tf.float32)
-    input_tensor = tf.image.resize(input_tensor, [32,32])
-    input_tensor = tf.expand_dims(input_tensor, 0)
 
 
-    # Make a prediction
-    output = model(input_tensor)
+
+
+# Function for predicting
+def predict(image_tensor):
+    # Some prediction code goes here
+    output = model(image_tensor)
     predictions = output.numpy().tolist()
 
-    # Return the prediction as a response
-    return {'predictions': predictions}
+    return predictions
+
+
+
+
+# Functions for preprocessing the image
+def preproc_img(image):
+    input_tensor = tf.image.decode_image(image)
+    input_tensor = tf.image.convert_image_dtype(input_tensor, tf.float32)
+    input_tensor = tf.image.resize(input_tensor, IMG_SIZE)
+    input_tensor = tf.expand_dims(input_tensor, 0)
+
+    return input_tensor
+
+
+
+
+# Function for returning the prediction of image 
+def predict_image(image):
+    image_tensor = preproc_img(image)
+    result = predict(image_tensor)
+    return max(result)
+
+
+
+# Defining the FastAPI instance here
+app = FastAPI()
+
+@app.root('/')
+async def root_func():
+    return {'message': 'this is the root function'}
+
+@app.post('/predict_image')
+async def upload_image(image: UploadFile):
+    try:
+        result = predict_image(await image.read())
+    except Exception as e:
+        print(e) 
+        result = "null"
+
+    return {'prediction': result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=APP_HOST, port=APP_PORT)
